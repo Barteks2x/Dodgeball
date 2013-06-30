93dd1486a2534b83c558b5b30a193a854b23dff7
Barteks2x <Bartek_s2@wp.pl>
2013-06-30 19:26:38 +0200

    first commit

---
+++ src/main/java/org/barteks2x/minielementgaming/Plugin.java
@@ -0,0 +1,18 @@
+package org.barteks2x.minielementgaming;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("This is bukkit plugin!");
+	}
+}
